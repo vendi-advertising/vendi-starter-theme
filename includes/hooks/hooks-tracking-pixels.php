4c41dff1e448570fac6b2fd7ed6bbca1e93e444e
Chris Haas <chris@vendiadvertising.com>
2018-10-04 17:10:53 -0500

    Update hooks-tracking-pixels.php

--- includes/hooks/hooks-tracking-pixels.php
+++ includes/hooks/hooks-tracking-pixels.php
@@ -4,5 +4,14 @@ add_action(
             'wp_head',
             function()
             {
+
             }
-           );
+        );
+
+add_action(
+            'vendi/just-after-opening-body-tag/tracking-pixel',
+            function()
+            {
+
+            }
+        );

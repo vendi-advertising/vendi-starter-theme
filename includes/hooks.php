9b9193ecde9b1cf2913bcfd5e71df4acdacb8946
Chris Haas <chris@vendiadvertising.com>
2018-10-04 17:13:12 -0500

    Update hooks.php

--- includes/hooks.php
+++ includes/hooks.php
@@ -3,4 +3,4 @@
 require_once CLIENT_THEME_PATH . '/includes/hooks/hooks-gravity.php';
 require_once CLIENT_THEME_PATH . '/includes/hooks/hooks-wordpress.php';
 require_once CLIENT_THEME_PATH . '/includes/hooks/hooks-cpts.php';
-
+require_once CLIENT_THEME_PATH . '/includes/hooks/hooks-tracking-pixels.php';

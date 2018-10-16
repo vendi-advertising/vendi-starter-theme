d3a60d26a863bd334f998c0e1cfc36333e6fb40b
Chris Haas <chris@vendiadvertising.com>
2018-10-16 16:11:18 -0500

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -9,12 +9,12 @@
         <a id="main-content" tabindex="-1"></a>
 
         <?php
-            $cc_path = Path::join(get_template_directory(),  . 'page-parts', 'content-components.php');
+            $cc_path = Path::join(get_template_directory(),  'page-parts', 'content-components.php');
             if(is_readable($cc_path)){
                 include $cc_path;
             }
 
-            $ml_path = Path::join(get_template_directory(),  . 'page-parts', 'main-loop.php');
+            $ml_path = Path::join(get_template_directory(),  'page-parts', 'main-loop.php');
             if(is_readable($ml_path)){
                 include $ml_path;
             }

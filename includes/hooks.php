57d6ceccd836064dff3145ae592cb93469a00b8e
Chris Haas <chris@vendiadvertising.com>
2018-10-16 16:06:09 -0500

    Update hooks.php

--- includes/hooks.php
+++ includes/hooks.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once CLIENT_THEME_PATH . '/includes/hooks/hooks-gravity.php';
-require_once CLIENT_THEME_PATH . '/includes/hooks/hooks-wordpress.php';
-require_once CLIENT_THEME_PATH . '/includes/hooks/hooks-cpts.php';
-require_once CLIENT_THEME_PATH . '/includes/hooks/hooks-tracking-pixels.php';
+require_once %VENDI_CUSTOM_THEME%_PATH . '/includes/hooks/hooks-gravity.php';
+require_once %VENDI_CUSTOM_THEME%_PATH . '/includes/hooks/hooks-wordpress.php';
+require_once %VENDI_CUSTOM_THEME%_PATH . '/includes/hooks/hooks-cpts.php';
+require_once %VENDI_CUSTOM_THEME%_PATH . '/includes/hooks/hooks-tracking-pixels.php';

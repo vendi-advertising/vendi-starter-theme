800f7da1c6a2754e4241a99f080e2cd277eacce9
C.J. Schrunk <cj@vendiadvertising.com>
2019-04-29 13:58:05 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -36,14 +36,14 @@
                         <div class="footer-bottom-right">
                             <div class="footer-social">
                                 <ul>
-                                    <li><a href="#" class="facebook_account"><i class="fab fa-facebook-f fa-fw"></i></a></li>
-                                    <li><a href="#" class="twitter_account"><i class="fab fa-twitter fa-fw"></i></a></li>
-                                    <li><a href="#" class="google_plus_account"><i class="fab fa-google-plus-g fa-fw"></i></a></li>
-                                    <li><a href="#" class="linkedin"><i class="fab fa-linkedin-in fa-fw"></i></a></li>
+                                    <li><a target="_blank" href="#" class="facebook_account"><i class="fab fa-facebook-f fa-fw"></i></a></li>
+                                    <li><a target="_blank" href="#" class="twitter_account"><i class="fab fa-twitter fa-fw"></i></a></li>
+                                    <li><a target="_blank" href="#" class="linkedin"><i class="fab fa-linkedin-in fa-fw"></i></a></li>
+				    <li><a target="_blank" href="#" class="youtube"><i class="fab fa-youtube fa-fw"></i></a></li>
                                 </ul>
                             </div>
                             <div class="footer-copyright">
-                                Copyright &copy; <?php echo date( 'Y ' ); ?> COMPANY, Inc.
+                                Copyright &copy; <?php echo date( 'Y ' ); bloginfo( 'name' ); ?>
                             </div>
                         </div>
 					</div>

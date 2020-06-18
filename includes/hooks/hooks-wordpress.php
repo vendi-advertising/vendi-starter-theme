b31e1cf938c586286e58112e5afba9d11a90c97e
C.J. Schrunk <cj@vendiadvertising.com>
2020-06-18 10:46:20 -0500

    Add responsive video container.

--- includes/hooks/hooks-wordpress.php
+++ includes/hooks/hooks-wordpress.php
@@ -24,3 +24,13 @@ add_action(
 
     }
 );
+
+/*  Add responsive container to embeds
+/* ------------------------------------ */
+function alx_embed_html($html)
+{
+    return '<div class="video-container">' . $html . '</div>';
+}
+
+add_filter('embed_oembed_html', 'alx_embed_html', 10, 3);
+add_filter('video_embed_html', 'alx_embed_html');

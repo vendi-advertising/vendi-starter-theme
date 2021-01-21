e41f8aa2cb5bdabb3e65047d88055ddd2c2c352c
Chris Haas <chris@vendiadvertising.com>
2021-01-21 13:25:03 -0600

    Support non-standard installs for search queries

--- searchform.php
+++ searchform.php
@@ -3,7 +3,7 @@
  * Template for displaying search forms
  */
 ?>
-    <form role="search" method="get" class="search-form" action="/">
+    <form role="search" method="get" class="search-form" action="<?php esc_attr_e(home_url('/')); ?>">
         <label>
             <span class="screen-reader-text">Search for:</span>
             <input type="search" class="search-field" placeholder="Search …" value="" name="s" />

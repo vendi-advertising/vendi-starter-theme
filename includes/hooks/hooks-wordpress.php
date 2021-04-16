12122a0a1b43b997c0c78d85208947129334ade3
Chris Haas <chris@vendiadvertising.com>
2021-04-16 09:30:22 -0500

    Added more recent options for add_theme_support for HTML5

--- includes/hooks/hooks-wordpress.php
+++ includes/hooks/hooks-wordpress.php
@@ -8,7 +8,7 @@ add_action(
         //Add featured image support
         add_theme_support('post-thumbnails');
         add_theme_support('title-tag');
-        add_theme_support('html5', ['comment-list', 'comment-form', 'search-form', 'gallery', 'caption']);
+        add_theme_support('html5', array('comment-list', 'comment-form', 'search-form', 'gallery', 'caption', 'style', 'script'));
 
         //Register some menus
         register_nav_menus(

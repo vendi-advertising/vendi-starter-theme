a1fb8c17247eaf9113344f96abbce7871049e0b2
Sarah Lindblom <sarahl@vendiadvertising.com>
2022-07-19 10:01:34 -0500

    adding pwd protection to index

--- index.php
+++ index.php
@@ -6,20 +6,23 @@ get_header();
         <a id="main-content" tabindex="-1"></a>
 
         <?php
+        if (post_password_required() ) {
+            echo get_the_password_form();
 
-        vendi_load_site_component('hero');
-        vendi_load_site_component('breadcrumbs');
+        } else {
+            vendi_load_site_component('hero');
+            vendi_load_site_component('breadcrumbs');
 
-        if (is_404()) {
-            vendi_load_page_component('404');
-        } elseif (is_search()) {
-            vendi_load_page_component('search');
+            if (is_404()) {
+                vendi_load_page_component('404');
+            } elseif (is_search()) {
+                vendi_load_page_component('search');
+            }
+
+            if (!is_search() && !is_404()) {
+                vendi_load_site_component('components');
+            }
         }
-
-        if (!is_search() && !is_404()) {
-            vendi_load_site_component('components');
-        }
-
         ?>
 
     </main>

c713c8ac450bbb52e965e102913bf3521f5c8a2c
Chris Haas <chris@vendiadvertising.com>
2022-07-27 08:10:55 -0500

    Create hooks-acfe.php

---
+++ includes/hooks/hooks-acfe.php
@@ -0,0 +1,15 @@
+<?php
+
+// Or using acfe/init
+add_action(
+    'acfe/init',
+    static function (){
+      
+      // Please perform all CPT and taxonomy work in the YAML files
+      acfe_update_setting('modules/post_types', false);
+      acf_update_setting('acfe/modules/taxonomies', false);
+    
+      // Please register all options in PHP
+      acfe_update_setting('modules/options_pages', false);
+  }
+);

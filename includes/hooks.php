728732e28ccc10fcbe07243a479c10a9ce2e7184
Chris Haas <chris@vendiadvertising.com>
2022-07-27 08:11:15 -0500

    Update hooks.php

--- includes/hooks.php
+++ includes/hooks.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once VENDI_CUSTOM_THEME_PATH . '/includes/hooks/hooks-acf.php';
+require_once VENDI_CUSTOM_THEME_PATH . '/includes/hooks/hooks-acfe.php';
 require_once VENDI_CUSTOM_THEME_PATH . '/includes/hooks/hooks-gravity.php';
 require_once VENDI_CUSTOM_THEME_PATH . '/includes/hooks/hooks-wordpress.php';
 require_once VENDI_CUSTOM_THEME_PATH . '/includes/hooks/hooks-cpts.php';

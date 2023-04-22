6a8e5a6983fbe4068ab868bad696fd955bba978b
Chris Haas <chris@vendiadvertising.com>
2023-04-22 07:22:59 -0500

    Update global-functions.php

--- includes/global-functions.php
+++ includes/global-functions.php
@@ -1,3 +1,41 @@
 <?php
 
-//Please avoid using global functions if possible!
+/**
+ * Enqueue a style and automatically call filemtime on the file to get the version number.
+ *
+ * @param string $handle
+ * @param string $srcRelativeToThemeRoot
+ * @param array|bool|null $deps
+ * @param string $media
+ * @return void
+ */
+function vendi_theme_enqueue_style(string $handle, string $srcRelativeToThemeRoot, array|bool|null $deps = [], string $media = 'all'): void
+{
+    wp_enqueue_style(
+        handle: $handle,
+        src: VENDI_CUSTOM_THEME_URL.$srcRelativeToThemeRoot,
+        deps: $deps,
+        ver: filemtime(VENDI_CUSTOM_THEME_PATH.$srcRelativeToThemeRoot),
+        media: $media
+    );
+}
+
+/**
+ * Enqueue a script and automatically call filemtime on the file to get the version number.
+ *
+ * @param string $handle
+ * @param string $srcRelativeToThemeRoot
+ * @param array|bool|null $deps
+ * @param bool $in_footer
+ * @return void
+ */
+function vendi_theme_enqueue_script(string $handle, string $srcRelativeToThemeRoot, array|bool|null $deps = [], bool $in_footer = false): void
+{
+    wp_enqueue_script(
+        handle: $handle,
+        src: VENDI_CUSTOM_THEME_URL.$srcRelativeToThemeRoot,
+        deps: $deps,
+        ver: filemtime(VENDI_CUSTOM_THEME_PATH.$srcRelativeToThemeRoot),
+        in_footer: $in_footer
+    );
+}

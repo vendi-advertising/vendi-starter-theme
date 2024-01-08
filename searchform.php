47039494523b99d388e5cbb51bba2275e86127b3
Chris Haas <chris@vendiadvertising.com>
2024-01-08 14:43:32 -0600

    Remove old theme utilities

--- searchform.php
+++ searchform.php
@@ -3,10 +3,10 @@
  * Template for displaying search forms
  */
 ?>
-    <form role="search" method="get" class="search-form" action="<?php esc_attr_e(home_url('/')); ?>">
-        <label>
-            <span class="screen-reader-text">Search for:</span>
-            <input type="search" class="search-field" placeholder="Search …" value="" name="s" />
-        </label>
-        <input type="submit" class="search-submit" value="Search" />
-    </form>
+<form role="search" method="get" class="search-form" action="<?php esc_attr_e(home_url('/')); ?>">
+    <label>
+        <span class="screen-reader-text">Search for:</span>
+        <input type="search" class="search-field" placeholder="Search …" value="" name="s"/>
+    </label>
+    <input type="submit" class="search-submit" value="Search"/>
+</form>

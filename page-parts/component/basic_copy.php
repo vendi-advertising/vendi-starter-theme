9a88bdbf4a3b2cd05d8cdd9221f4c376ded614d2
Chris Haas <chris@vendiadvertising.com>
2024-01-08 09:42:06 -0600

    Basic copy, reset up, prefer where over is

---
+++ page-parts/component/basic_copy.php
@@ -0,0 +1,8 @@
+<section
+    class="component-basic-copy"
+    <?php vendi_maybe_get_row_id_attribute_from_subfield(); ?>
+>
+    <div class="region">
+        <?php echo get_sub_field('copy') ?>
+    </div>
+</section>
